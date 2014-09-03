ea25f9502efe8eacf76738e7dfad9cd48304a7f0
Bing.L <bing-l@outlook.com>
2014-09-03 17:41:53 +0800

    update main.go

--- main.go
+++ main.go
@@ -14,13 +14,13 @@ import (
 var (
 	syncdb = flag.Bool("syncdb", false, "\t同步数据库结构")
 	force  = flag.Bool("force", false, "\tsyncdb 的强制模式，会丢失所有数据")
-	run    = flag.Bool("run", true, "\t\t运行系统")
+	run    = flag.Bool("run", false, "\t\t运行系统")
 )
 
 func main() {
 	flag.Parse()
 	if flag.NFlag() == 0 { //没有使用参数就打印帮助
-		fmt.Println("命令行参数：")
+		fmt.Println("命令行参数及默认值：")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
